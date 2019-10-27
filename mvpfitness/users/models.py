8227b81fcdc6e602f7736a9599640984b0462ddf
Nishant <nishant@P4I.local>
2019-10-27 20:47:44 +0400

    rfegistration and login page

--- mvpfitness/users/models.py
+++ mvpfitness/users/models.py
@@ -3,12 +3,55 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 # Create your models here.
 
 
+class SocialMedia(models.Model):
+    id = models.CharField(primary_key=True, editable=False, max_length=25)
+    social_platform = models.TextField(max_length=50)
+    session_id = models.TextField(max_length=70)
+
+
+class Address(models.Model):
+    id = models.CharField(primary_key=True, editable=False, max_length=25)
+    street_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=70)
+    state = models.CharField(max_length=50, blank=True)
+    city =  models.CharField(max_length=30)
+    zip_code = models.PositiveIntegerField()
+
+
+class Tier(models.Model):
+    id = models.CharField(primary_key=True, editable=False, max_length=25)
+    tier_plan = models.CharField(max_length=30)
+    tier_amount = models.PositiveIntegerField()
+    valid_until = models.DateTimeField()
+
+
 class User(AbstractBaseUser):
-    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
-    username = models.TextField(verbose_name='username', max_length=255, default='')
-    first_name = models.TextField(verbose_name='first_name', max_length=255, default='')
-    last_name = models.TextField(verbose_name='last_name', max_length=255, default='')
-    password = models.TextField(verbose_name='last_name',max_length=50, default='')
+    id = models.CharField(primary_key=True, editable=False, max_length=25)
+    social_media_id = models.ForeignKey(SocialMedia, on_delete=models.CASCADE)
+    address_id = models.ForeignKey(Address, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.TextField(max_length=10)
+    email = models.EmailField(max_length=70)
+    password = models.CharField(max_length=50)
+    user_type = models.CharField(max_length=15)
+    timezone = models.CharField(max_length=25)
+    created_at = models.DateTimeField()
+    cancelled_at = models.DateTimeField(default='')
+    date_of_birth = models.DateField()
+    suspended_until = models.DateTimeField(default='')
+    gender = models.CharField(max_length=20)
+    last_attended_date = models.DateTimeField(default='')
+    last_login_at = models.DateTimeField(default='')
+    tier_id = models.ForeignKey(Tier, on_delete=models.CASCADE)
+    account_status = models.CharField(max_length=30)
+    password_reminder_token = models.CharField(max_length=100)
+    password_remider_expire = models.CharField(max_length=120)
+    email_confirmation_token = models.CharField(max_length=150)
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    session_count = models.PositiveIntegerField()
+    telephone = models.CharField(max_length=20)
 
     def create_user(self, email, first_name, last_name,username, password):
         if not email:
@@ -24,6 +67,7 @@ class User(AbstractBaseUser):
 
     def get_short_name(self):
         return self.username
+
 
     def __str__(self):
         return self.email
